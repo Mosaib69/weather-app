109d723ffaf5188f125344fb724ae74d4b7957d0
Mosaib Ali Khan <mosaibkhan422@gmail.com>
2024-05-13 14:26:31 +0530

    Added script file

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputField = document.querySelector("#input-field");
+const btn = document.querySelector("#btn");
+const degree = document.querySelector("#degree");
+const city = document.querySelector("#city");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#wind-speed");
